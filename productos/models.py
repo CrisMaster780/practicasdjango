347fce362faa5d9942fee9bbe05f9ac155ec8e4c
CrisMaster780 <cristobalgerardo2611@gmail.com>
2023-03-07 20:19:30 -0300

    commit

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Impuesto(models.Model):
+    descripcion = models.CharField(max_length=200)
+    porcentaje = models.FloatField()
+
+
+class Productos(models.Model):
+    codigo_fabricante = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
+    precio_costo = models.FloatField()
+    precio_venta= models.FloatField()
+    existencia = models.IntegerField
+    cantidad_minima = models.IntegerField
+    cantidad_maxima = models.IntegerField
+    impuesto = models.ForeignKey(Impuesto, on_delete=models.CASCADE)
+    estado = models.BooleanField(default=True)
