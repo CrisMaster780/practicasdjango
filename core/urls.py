ce8a77030663fd157695b4ba75369c3d1ddc6c8c
CrisMaster780 <cristobalgerardo2611@gmail.com>
2023-03-07 20:35:16 -0300

    commit_1

--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls'), name='Principal'),
-    path('productos/', include('productos.urls'), name='Productos'),
+    path('', include('productos.urls'), name='Productos'),
 ]
