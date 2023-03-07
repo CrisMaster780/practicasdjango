0a810ded64cf2c5ebf50bb1a260446a4fa14822c
CrisMaster780 <cristobalgerardo2611@gmail.com>
2023-03-07 07:27:00 -0300

    commit_HU_01

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.Index, name='Index'),
+    path('principal/', views.Principal, name='Principal'),
+    
 ]+ static(settings.STATIC_URL)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 def Index(request):
-    print(request.POST)
-    return render(request, 'base/login.html')
+    if request.method == 'GET':
+        return render(request, 'base/login.html')
+    else:
+        _username = request.POST.get('username')
+        _password = request.POST.get('password')
+
+        if _username is None or _password is None:
+            print('entro aca')
+            error = 'Ingrese todos los datos'
+
+            return render(request, 'base/login.html', {
+                'error': error
+            })
+        else:
+
+
+            return redirect(Principal)
+
+
+def Principal(request):
+    return render(request, 'base/index.html', {
+               
+            })
+
+
+   
