a0daccaef9c550c44b2d5e8f782a4e2df061cb2a
CrisMaster780 <cristobalgerardo2611@gmail.com>
2023-03-07 20:39:58 -0300

    commit_2

--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from productos.models import Productos
 
 def Index(request):
+    _productos = Productos.objects.all()
+
     return render(request, 'productos/index.html')
