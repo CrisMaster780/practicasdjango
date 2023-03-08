afcce9595738d9bfcd41fd9c3f12aaa8c5333aa6
cristoballopez <cristobal.lopez@upacifico.edu.py>
2023-03-08 06:43:11 -0300

    commit_3

--- productos/models.py
+++ productos/models.py
@@ -13,7 +13,7 @@ class Productos(models.Model):
     precio_costo = models.FloatField()
     precio_venta= models.FloatField()
     existencia = models.IntegerField()
-    cantidad_minima = models.IntegerField
-    cantidad_maxima = models.IntegerField
+    cantidad_minima = models.IntegerField()
+    cantidad_maxima = models.IntegerField()
     impuesto = models.ForeignKey(Impuesto, on_delete=models.CASCADE)
     estado = models.BooleanField(default=True)
