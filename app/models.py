639d53eead719a60464436ec446c2900d78d56b2
CrisMaster780 <95488614+CrisMaster780@users.noreply.github.com>
2023-03-11 20:13:33 +0100

    commit_5

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Sucursal(models.Model):
+    nombre = models.CharField(max_length=200)
+    direccion= models.CharField(max_length=200)
+    telefono= models.CharField(max_length=200)
+    correo = models.CharField(max_length=200)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    estado = models.BooleanField(default=True)
+
--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from app.models import Sucursal
+from django.contrib.auth.models import User
 
 
 def Index(request):
@@ -7,24 +10,30 @@ def Index(request):
     else:
         _username = request.POST.get('username')
         _password = request.POST.get('password')
-
-        if _username is None or _password is None:
-            print('entro aca')
-            error = 'Ingrese todos los datos'
-
+        user = authenticate(request, username=_username, password=_password)
+        if user is None:
             return render(request, 'base/login.html', {
-                'error': error
+                'error': 'USUARIO O CONTRASENA INCORRECTOS'
             })
         else:
+            login(request, user)
 
-
-            return redirect(Principal)
+    return redirect(Principal)
 
 
 def Principal(request):
-    return render(request, 'base/index.html', {
-               
-            })
+    usuario = request.user.username
+    id_usu = request.user.id
+    estr = Sucursal.objects.filter(usuario__id__contains=id_usu).first()
+    titulo = 'Principal'
+    return render(request, 'base/index.html',  {
+        'usuario': usuario,
+        'titulo': titulo,
+        'sucursal': estr
+
+    })
 
 
-   
+def salir(request):
+    logout(request)
+    return redirect("/")
